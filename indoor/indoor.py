b5a61c0c24b984c0ea087e825212d0d34747f8cc
AdamaDiallo <adama7.diallo.pro@gmail.com>
2026-03-31 13:40:14 +0200

    Problem Set 0: Indoor Voice

---
+++ indoor/indoor.py
@@ -0,0 +1,5 @@
+# Ce programme affiche le texte entré par l'utilisateur en minuscules
+text = input ("écrivez n'importe quoi pour continuer")
+
+# Affiche le texte en minuscules
+print(text.lower())
