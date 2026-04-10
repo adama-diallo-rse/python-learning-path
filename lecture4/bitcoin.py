16a2e3deb375c4fcfc62287df892f1cd1f367c97
Adama Diallo <adama7.diallo.pro@gmail.com>
2026-04-10 18:54:03 +0200

    Add Lecture 4 problem sets: emojize, figlet, adieu, game, bitcoin

---
+++ lecture4/bitcoin.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument")
+
+try:
+    n = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+try:
+    response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=TA_CLE_API")
+    data = response.json()
+    price = float(data["data"]["priceUsd"])
+    total = price * n
+    print(f"${total:,.4f}")
+except requests.RequestException:
+    sys.exit("Request failed")
